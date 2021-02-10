ba97c1a3017cbd3e77f1a1c744cc4c7a3e36b7f1
Catherine Lan <yl4289@columbia.edu>
2021-02-11 04:26:20 +0800

    made changes to the setup file

--- setup.py
+++ setup.py
@@ -10,8 +10,13 @@ from setuptools import setup
 setup(
     name="menu",
     version="0.0.1",
-    packages=[],
+    author="Catherine Lan",
+    author_email="yl4289@columbia.edu",
+    license="GPLv3",
+    description="A package for deciding what to eat",
+    classifiers=["Programming Language :: Python :: 3"],
     entry_points={
-        "console_scripts": ["test = menu.__main__:main"]
+        # "console_scripts": ["module = package/directory.__main__:main"]
+        "console_scripts": ["foodmenu = test.__main__:main"]
     },
 )
