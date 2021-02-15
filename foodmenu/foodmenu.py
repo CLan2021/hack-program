065433ca9038d13d08ed8ca5e522bc6f5d9a23f5
Catherine Lan <yl4289@columbia.edu>
2021-02-16 03:40:04 +0800

    cleaned code

--- foodmenu/foodmenu.py
+++ foodmenu/foodmenu.py
@@ -10,18 +10,6 @@ import pandas as pd
 URL  = "https://raw.githubusercontent.com/CLan2021/hack-program/main/menu_option.csv"
 df = pd.read_csv(URL, sep=",")
 
-#class WhatToEat(option):
-	#def __init__(self, name, price):
-        #self.data = pd.read_csv(FILE, sep=",")
-		#self.name = name
-        #self.price = price
-	
-	#def getprice(self):
-		#return self.price
-	
-	#def __str__(self):
-        #return self.name + ' : ' + str(self.getprice())
-	
 def menu(arg):
 
 	"""
@@ -58,31 +46,3 @@ if __name__ == "__main__":
 	menu("lunch")
 	menu("dinner")
 	option()
-
-
-	
-	#def run(self, arg):
-		#"""
-		#Put it all together into a single function call that returns
-		#"""
-		#self.menu(arg)
-		#return self.data
-
-#breakfast = [
-    #"chocolate pancakes and coffee",
-    #"omelette and juice",
-    #"cereal and milk"
-#]
-
-#lunch = [
-    #"egg and avocado sandwich",
-    #"chipotle bowl",
-    #"chicken caesar salad"
-#]
-
-#dinner = [
-	#"quinoa with chicken and lentils",
-	#"shrimp pad thai",
-	#"beef pho"
-#]
-
