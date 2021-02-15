e301d119f900dee13877160e7999c40e5e6a7565
Catherine Lan <yl4289@columbia.edu>
2021-02-16 03:49:59 +0800

    updated the result setting

--- foodmenu/foodmenu.py
+++ foodmenu/foodmenu.py
@@ -10,6 +10,9 @@ import pandas as pd
 URL  = "https://raw.githubusercontent.com/CLan2021/hack-program/main/menu_option.csv"
 df = pd.read_csv(URL, sep=",")
 
+# make sure sentence doesn't get cut off 
+pd.set_option("display.max_colwidth", 100)
+
 def menu(arg):
 
 	"""
